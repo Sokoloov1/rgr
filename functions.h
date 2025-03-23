efe086c82d19cd51787649807174712dae9dc4fd
Sokoloov1 <167451365+Sokoloov1@users.noreply.github.com>
2025-03-23 19:17:42 +0700

    functions.h

---
+++ functions.h
@@ -0,0 +1,22 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+
+void gronsfeld(string data);
+void cezar(string data);
+void atbash(string data);
+
+extern int act_cel;
+extern string type_inp;
+extern int LEN;
+#endif
