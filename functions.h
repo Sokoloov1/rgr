ac26867c504b399b90425ee3d7a405674868b878
Sokoloov1 <167451365+Sokoloov1@users.noreply.github.com>
2025-04-01 17:45:16 +0700

    functions.h

--- functions.h
+++ functions.h
@@ -8,7 +8,8 @@ void atbash(string& data);
 void cezar(string& data);
 void gronsfeld(string& data);
 
-extern int act_cel;  // 0=шифрование, 1=дешифрование
-extern string type_inp;  // "1"=консоль, "2"=файл
+extern int act_cel;  // 0 - шифрование, 1 - дешифрование
+extern string type_inp;  // "1" - консоль, "2" - файл
+extern const int LEN;  // Диапазон символов (126 для печатных ASCII)
 
 #endif
